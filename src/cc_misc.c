dd9edd7908499f947f01c35bc3267c07de71443a
Anderson Lentz da Silva <anderlentz@gmail.com>
2017-09-07 22:23:59 -0300

    Funçao de imprimir tabela de simbolos implementada.

--- src/cc_misc.c
+++ src/cc_misc.c
@@ -1,9 +1,28 @@
 #include "cc_misc.h"
 #include "cc_dict.h"
+#include <main.h>
 
 int line_count = 1;
 
-comp_dict_t *symbol_table; 
+/*
+* Tabela de simbolos onde sao armazenados os tokens
+* Item 1.3 , etapa 1
+*/
+comp_dict_t *symbol_table;	
+
+/*
+* Percorre os itens do ponteiro de overflow da 
+* da estrutura {comp_dict_item} e imprime sua key e 
+* valor(linha onde ocorreu o lexema)
+*/
+void dict_item_list_print(comp_dict_item_t * item)
+{
+  while (item != NULL) {
+    cc_dict_etapa_1_print_entrada(item->key,item->value);
+    item = item->next;
+  }
+}
+
 
 int comp_get_line_number (void)
 {
@@ -18,23 +37,34 @@ void yyerror (char const *mensagem)
 
 void main_init (int argc, char **argv)
 {
-  //implemente esta função com rotinas de inicialização, se necessário
+ 	//implemente esta função com rotinas de inicialização, se necessário
 
-	
 	symbol_table = dict_new();		//Cria tabela de simbolos
-	//printf("Criado tabela de simbolos\n\n");
+	
 }
 
 void main_finalize (void)
 {
 
-	//dict_debug_print(symbol_table);
-  
 }
 
 void comp_print_table (void)
 {
-  //para cada entrada na tabela de símbolos
-  //Etapa 1: chame a função cc_dict_etapa_1_print_entrada
-  //implemente esta função
+ 	
+	printf("Imprimindo tabela de simbolos\n" );
+	
+	for (int i = 0; i < symbol_table->size; ++i) {
+	    if (symbol_table->data[i]) {
+
+	      	cc_dict_etapa_1_print_entrada(symbol_table->data[i]->key, symbol_table->data[i]->value);
+
+	      	if (symbol_table->data[i]->next)	//entra aqui se ocorreu um caso de overflow
+	      		dict_item_list_print(symbol_table->data[i]->next);
+	     	 
+	    }
+	    
+  	}
+	
 }
+
+
